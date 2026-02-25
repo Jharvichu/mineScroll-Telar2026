97367e0d7b053821ce0c69e4e96342e22f407844
Jharvy <jharvy.cadillo.t@uni.pe>
2026-02-24 22:06:00 -0500

    feat: Agregar scripts del paquete de maquina de estados

--- Assets/_Project/Scripts/StateMachine/AState.cs
+++ Assets/_Project/Scripts/StateMachine/AState.cs
@@ -10,15 +10,12 @@ namespace StateMachine {
 		protected bool _leftInput;
 		protected bool _upInput;
 		protected bool _downInput;
-		protected ActionData _actionPerformed;
 
 		public AState(SO_State data) {
 			_stateData = data;
 		}
 
-		public virtual void EnterState() {
-			InputHandler.Instance.actionTrigger.AddListener(GetActionNInputs);
-		}
+		public virtual void EnterState() { }
 
 		public virtual void ExitState() { }
 
@@ -31,21 +28,5 @@ namespace StateMachine {
 			_parent = parent;
 		}
 
-		private void GetActionNInputs(ActionData data) {
-			_rightInput = data.inputs.Exists(element => {
-				return element.button == Btn.Right && element.canHold;
-			});
-			_leftInput = data.inputs.Exists(element => { return element.button == Btn.Left && element.canHold; });
-			_upInput = data.inputs.Exists(element => { return element.button == Btn.Up && element.canHold; });
-			_downInput = data.inputs.Exists(element => { return element.button == Btn.Down && element.canHold; });
-
-			_actionPerformed = data;
-
-			// if (Input.GetKeyDown(KeyCode.S))
-			// {
-			//	_downInput = !_downInput;
-			// }
-
-		}
 	}
 }
