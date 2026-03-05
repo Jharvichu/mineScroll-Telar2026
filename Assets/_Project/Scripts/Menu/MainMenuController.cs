a13b5a1284c4e4bc3656519c8930b044e5286628
Alvaro Ivan Roldan Candiotti <aroldan750@gmail.com>
2026-03-05 13:08:37 -0500

    Cambios de angie
    tqm

--- Assets/_Project/Scripts/Menu/MainMenuController.cs
+++ Assets/_Project/Scripts/Menu/MainMenuController.cs
@@ -1,58 +1,33 @@
-using UnityEngine;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using System.Collections;
-using FMODUnity;
 
 public class MainMenuController : MonoBehaviour
 {
     public GameObject transition;
     public float transitionTime = 1f;
 
-    [SerializeField] EventReference select;
-    [SerializeField] EventReference back;
-    [SerializeField] EventReference hover1;
-    [SerializeField] EventReference hover2;
-    [SerializeField] EventReference hover3;
-    [SerializeField] EventReference hover4;
-
-
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        root.Q<Button>("PlayButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover1));
-
         root.Q<Button>("PlayButton").clicked += () =>
         {
-            AudioManager.Instance.PlaySFX(select);
             StartCoroutine(Transicion("PrologoScene"));
         };
 
-        root.Q<Button>("CreditsButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover3));
-
         root.Q<Button>("CreditsButton").clicked += () =>
         {
-            AudioManager.Instance.PlaySFX(select);
             StartCoroutine(Transicion("CreditsScene"));
         };
 
-        root.Q<Button>("ControlsButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover2));
-
         root.Q<Button>("ControlsButton").clicked += () =>
         {
-            AudioManager.Instance.PlaySFX(select);
             StartCoroutine(Transicion("ControlsScene"));
         };
 
-        root.Q<Button>("ExitButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover4));
-
-        root.Q<Button>("ExitButton").clicked += () =>
-        {
-            AudioManager.Instance.PlaySFX(select);
-            Application.Quit();
-            Debug.Log("Salí del juego");
-        };
+     
     }
 
     IEnumerator Transicion(string escena)
