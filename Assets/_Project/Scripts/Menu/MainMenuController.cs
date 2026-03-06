b7c92a7d721f06f6c935f676646b48db2dead6ac
Angie Mina Ishuiza <107209336+AngieMinaI@users.noreply.github.com>
2026-03-06 03:19:18 -0500

    UpdateSonidos

--- Assets/_Project/Scripts/Menu/MainMenuController.cs
+++ Assets/_Project/Scripts/Menu/MainMenuController.cs
@@ -1,40 +1,56 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using System.Collections;
+using FMODUnity;
 
 public class MainMenuController : MonoBehaviour
 {
     public GameObject transition;
     public float transitionTime = 1f;
 
+    //AUDIO
+    [SerializeField] EventReference select;
+    [SerializeField] EventReference back;
+    [SerializeField] EventReference hover1;
+    [SerializeField] EventReference hover2;
+    [SerializeField] EventReference hover3;
+
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
+        root.Q<Button>("PlayButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover1));
+
         root.Q<Button>("PlayButton").clicked += () =>
         {
+            AudioManager.Instance.PlaySFX(select);
             StartCoroutine(Transicion("PrologoScene"));
         };
 
+        root.Q<Button>("CreditsButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover3));
+
         root.Q<Button>("CreditsButton").clicked += () =>
         {
+            AudioManager.Instance.PlaySFX(select);
             StartCoroutine(Transicion("CreditsScene"));
         };
 
+        root.Q<Button>("ControlsButton").RegisterCallback<MouseEnterEvent>(_ => AudioManager.Instance.PlaySFX(hover2));
         root.Q<Button>("ControlsButton").clicked += () =>
         {
+            AudioManager.Instance.PlaySFX(select);
             StartCoroutine(Transicion("ControlsScene"));
         };
 
-     
+
     }
 
     IEnumerator Transicion(string escena)
     {
         transition.SetActive(true);
 
-        // Obtenemos el CanvasGroup del panel de transición
+        // Obtenemos el CanvasGroup del panel de transiciÃ³n
         CanvasGroup canvasGroup = transition.GetComponent<CanvasGroup>();
         if (canvasGroup == null)
             canvasGroup = transition.AddComponent<CanvasGroup>();
